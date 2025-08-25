60b37e730f94b2a299dd12ccda0a8e0561a95123
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-08-25 19:36:13 +0300

    Update `ProfileDetailsView` to use `Profile` model and `profile` context name

--- lash_store/accounts/views.py
+++ lash_store/accounts/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic as views
+
+from lash_store.accounts.models import Profile
 from lash_store.utils.user_mixins import GetProfileIdMixin
 
 User_model = get_user_model()
 
 class ProfileDetailsView(LoginRequiredMixin,GetProfileIdMixin,views.DetailView):
     template_name = 'accounts/profile_details.html'
-    model = User_model
-
+    model = Profile
+    context_object_name = 'profile'

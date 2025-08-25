69d9082cefb942f37202fd4862d4b297a72081b5
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-08-25 17:21:48 +0300

    Link `signals.py` import in `AccountsConfig.ready()` for signal registration

--- lash_store/accounts/apps.py
+++ lash_store/accounts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lash_store.accounts'
+    
+    def ready(self):
+        import lash_store.accounts.signals

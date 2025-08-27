32fc56a353b0f350816a63da347a437327a38986
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-08-27 19:57:37 +0300

    Add initial migration, URLs, and media handling for `product` app

---
+++ lash_store/product/urls.py
@@ -0,0 +1,9 @@
+from itertools import product
+
+from django.urls import path
+from lash_store.product.views import ListProductView, DetailProductView
+
+urlpatterns = [
+    path ('', ListProductView.as_view(), name='products'),
+    path('<slug:slug>/',DetailProductView.as_view() , name='product_details')
+]
--- lash_store/urls.py
+++ lash_store/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from lash_store.settings import DEBUG
 
 urlpatterns = [
     path("", include('lash_store.common.urls')),
@@ -25,3 +29,6 @@ urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
     path('product', include("lash_store.product.urls"))
 ]
+
+if DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

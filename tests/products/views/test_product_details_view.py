875c9222f89a7aac7c78cbc4f0b8f8ec3a15ea0b
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-09-02 19:26:31 +0300

    Add test for product list html

--- tests/products/views/test_product_details_view.py
+++ tests/products/views/test_product_details_view.py
@@ -1,21 +1,24 @@
+from django.test import TestCase
+from django.urls import reverse
+from lash_store.product.models import Product
+
 class DetailProductViewTests(TestCase):
     def setUp(self):
         self.product = Product.objects.create(
             name="Test Product",
-            slug="test-product",
             description="Test description",
             price=20.00,
             stock=10
         )
 
-    def test_detail_view_status_code(self):
-        response = self.client.get(reverse('product_detail', kwargs={'slug': self.product.slug}))
+    def test__detail_view_status_code_200(self):
+        response = self.client.get(reverse('product_details', kwargs={'slug': self.product.slug}))
         self.assertEqual(response.status_code, 200)
 
-    def test_detail_view_context(self):
-        response = self.client.get(reverse('product_detail', kwargs={'slug': self.product.slug}))
+    def test__detail_view_correct_context(self):
+        response = self.client.get(reverse('product_details', kwargs={'slug': self.product.slug}))
         self.assertEqual(response.context['object'], self.product)
 
-    def test_detail_view_404(self):
-        response = self.client.get(reverse('product_detail', kwargs={'slug': 'non-existent'}))
+    def test__detail_view_404(self):
+        response = self.client.get(reverse('product_details', kwargs={'slug': 'non-existent'}))
         self.assertEqual(response.status_code, 404)

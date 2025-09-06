c0d23192afb17eaa3d320207f1d0822ec211c0c1
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-09-06 20:31:56 +0300

    Add FBV add_to_cart_ajax to handle product add to cart message

--- lash_store/orders/views.py
+++ lash_store/orders/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views import generic as views
 
-# Create your views here.
+from lash_store.orders.models import Cart, CartItem
+from lash_store.product.models import Product
+
+
+class CartSummaryView(views.TemplateView):
+    template_name = 'orders/cart.html'
+
+cart_summary = CartSummaryView.as_view()
+
+def add_to_cart_ajax(request, product_id):
+    if request.method == "POST":
+        product = get_object_or_404(Product, pk=product_id)
+
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+        item.quantity += 1 #1 only for test TO BE REMOVED after test
+        item.save()
+
+        picture_url = product.images.first().image.url if product.images.exists() else ''
+
+        return JsonResponse({
+            "success": True,
+            "message": "Продуктът е добавен в кошницата",
+            "product_details": {
+                "name": product.name,
+                "price": str(product.price),
+                "quantity": item.quantity,
+                "picture": picture_url,
+                "slug": product.slug,
+            }
+        })
+    return JsonResponse({"success": False, "message": "Невалидна заявка"})

2103e8692ba23bab8263c89c212d5d4d808abbff
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-09-15 18:48:47 +0300

    Fix cyrillic slugify problem

--- lash_store/product/models.py
+++ lash_store/product/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils.text import slugify
+from slugify import slugify
 
 User_model = get_user_model()
 

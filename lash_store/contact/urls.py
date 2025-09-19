f187f43bf60e875129563b206fbfcafc904033e6
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-09-19 14:29:58 +0300

    Add FBV to handle AJAX message sending

--- lash_store/contact/urls.py
+++ lash_store/contact/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from lash_store.contact.views import contact_page
+from lash_store.contact.views import contact_page, send_contact_message
 
 urlpatterns = [
     path('',contact_page, name='contact-page'),
+    path('contact/send/', send_contact_message, name='send_contact_message'),
 ]
--- lash_store/contact/views.py
+++ lash_store/contact/views.py
@@ -1,7 +1,11 @@
+from django.http import JsonResponse
 from django.views import generic as views
+from django.views.decorators.http import require_POST
+from django.core.mail import send_mail
 
 from lash_store.contact.forms import ContactForm
 from lash_store.contact.models import ContactInfo, SocialLink
+from lash_store.settings import EMAIL_HOST_USER
 
 
 class ContactPageView(views.FormView):
@@ -24,3 +28,18 @@ class ContactPageView(views.FormView):
         return context
 
 contact_page = ContactPageView.as_view()
+
+@require_POST
+def send_contact_message(request):
+    form = ContactForm(request.POST)
+    if form.is_valid():
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        message = form.cleaned_data['message']
+
+        subject = f'Ново съобщение от {name} ({email})'
+        email_message = f'Име: {name}\nEmail: {email}\nСъобщение:\n{message}'
+        send_mail(subject,message =email_message,from_email=email,recipient_list = [EMAIL_HOST_USER],fail_silently=False)
+        return JsonResponse({'success': True, 'message': 'Съобщението е изпратено успешно!'})
+    else:
+        return JsonResponse({'success': False, 'errors': form.errors})
